582541cb46b22982a7ff6d3c00e548c4495c22f9
herreramelendez <127003756+herreramelendez@users.noreply.github.com>
2023-03-07 19:59:53 -0600

    Add files via upload

---
+++ Examen/Views/PageContacto.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Examen.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageContac : ContentPage
+    {
+        public PageContac()
+        {
+            InitializeComponent();
+        }
+        protected async void btnsalvar_Clicked(object sender, EventArgs e)
+        {
+            var con = new Models.Contacto
+            {
+                nombre = txtnombre.Text,
+                telefono = txttelefono.Text,
+                edad = edad.Text,
+                pais = cbpais.SelectedItem.ToString(),
+                nota = txtnota.Text,
+               
+            };
+
+
+            if (await App.DBCon.SaveCon(con) > 0)
+                await DisplayAlert("Aviso", "Contacto Ingresado", "OK");
+            else
+                await DisplayAlert("Aviso", "Error", "OK");
+
+            var page = new Views.PageResultado();
+            page.BindingContext = con;
+            await Navigation.PushAsync(page);
+
+        }
+    }
+}
---
+++ Examen/Views/PageInicial.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Examen.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageInitial : ContentPage
+    {
+        public PageInitial()
+        {
+            InitializeComponent();
+        }
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            var pagecontac = new Views.PageContac();
+            Navigation.PushAsync(pagecontac);
+        }
+        private void listaconta_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            listaconta.ItemsSource = await App.DBCon.GetListcon();
+
+        }
+
+        private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
